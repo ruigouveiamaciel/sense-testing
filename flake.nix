184b7c093a7deea20998429cb3604885d7c16722
Rui Maciel <ruigouveiamaciel@proton.me>
2024-03-24 21:57:53 +0000

    tauri app

--- flake.nix
+++ flake.nix
@@ -1,56 +1,87 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (nixpkgs) lib;
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [ playwright-driver.browsers ];
-                  
-                  env = {
-                    PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
-                    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
-                  };
+      rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      llvm = pkgs.llvmPackages_17;
 
-                  languages = {
-                    javascript = {
-                      enable = true;
-                      package = pkgs.nodejs_20;
-                      pnpm = {
-                        enable = true;
-                        package = pkgs.nodePackages.pnpm;
-                      };
-                    };
-                  };
-                }
-              ];
-            };
-          });
-    };
+      clangBuildInputs = with llvm; [
+        clang
+        libclang
+        libcxx
+        lld
+        lldb
+      ];
+
+      nativeBuildInputs = with pkgs;
+        [
+          pkg-config
+          rust-toolchain
+          cmake
+        ]
+        ++ clangBuildInputs;
+
+      libraries = with pkgs; [
+        webkitgtk
+        gtk3
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        openssl_3
+        librsvg
+      ];
+
+      packages = with pkgs; [
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        libsoup
+        webkitgtk
+        librsvg
+
+        playwright-driver.browsers
+        nodejs_20
+        nodePackages.pnpm
+      ];
+    in {
+      formatter = pkgs.alejandra;
+
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = nativeBuildInputs;
+        buildInputs = packages;
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+          export RUST_SRC_PATH=${rust-toolchain}
+          export RUST_BACKTRACE=1
+          export LIBCLANG_PATH="${llvm.libclang.lib}/lib";
+          export WEBKIT_DISABLE_COMPOSITING_MODE=1
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+        '';
+      };
+    });
 }
---
+++ packages/desktop/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let name = "";
+  let greetMsg = "";
+
+  async function greet() {
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<h1>Welcome to SvelteKit!</h1>
+<form on:submit|preventDefault={greet}>
+  <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+  <button type="submit">Greet</button>
+  <p>{greetMsg}</p>
+</form>
